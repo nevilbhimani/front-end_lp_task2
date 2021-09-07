01759ff24590e8550cfdc4f035afadf84147790e
nevilbhimani <bhimaninevil72@gmail.com>
2021-09-07 13:12:24 +0530

    commit 3
    using axios

--- task2.js
+++ task2.js
@@ -14,18 +14,27 @@
 // 	console.error(err);
 // });
 
-async function getData(){
-   let response = await fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
-      "method": "GET",
-      "headers": {
-         "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
-         "x-rapidapi-key": "891fd2d924msh4c1d3aed6b73804p13eaacjsn5a37fa64c771"
-      }
-   })
-   let data=await response.json()
-   return data;
-}
-getData().then((data) => {appendData(data)});
+// async function getData(){
+//    let response = await fetch("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
+//       "method": "GET",
+//       "headers": {
+//          "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
+//          "x-rapidapi-key": "891fd2d924msh4c1d3aed6b73804p13eaacjsn5a37fa64c771"
+//       }
+//    })
+//    let data=await response.json()
+//    return data;
+// }
+// getData().then((data) => {appendData(data)});
+
+axios.get("https://corona-virus-world-and-india-data.p.rapidapi.com/api", {
+   "method": "GET",
+   "headers": {
+      "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
+      "x-rapidapi-key": "891fd2d924msh4c1d3aed6b73804p13eaacjsn5a37fa64c771"
+   }
+}).then(response => {appendData(response.data)})
+.catch(err => {console.error(error)});
  function appendData(data) {
     var table = document.getElementById('myTable');
      
